25b4659b012b81779758a13b7296b0619062d48a
Christopher Ibarra <ibarrachristopher93@gmail.com>
2026-02-25 01:57:54 -0700

    Stop tracking deprecated directory

--- deprecated/scripts_bash/output_of_chart_structure_summary_jsons.bash
+++
@@ -1,7 +0,0 @@
-cd dir_chart_structure_quality_summary
-for file in *.json
-do
-echo $file
-cat $file
-done
-cd ../
--- deprecated/scripts_bash/python_code_dirs_display.bash
+++
@@ -1,6 +0,0 @@
-for dir in input_python_scripts module_aif_bar_reader output_python_scripts scripts_python; do
-	echo " "
-	echo "directory: $dir"
-	echo "contents: "
-	ls -1 "$dir"
-done
